275fab7c3a3bdbbadf61a8d7fcf686ac123f03f5
PvHoang1002 <hoang200340@gmail.com>
2023-11-14 22:03:40 +0700

    Update the usage method of environmet varialbe

--- weather.py
+++ weather.py
@@ -1,29 +1,28 @@
-from dotenv import load_dotenv
 from pprint import pprint
 import requests
 import os
 
-load_dotenv()
-
-
 def get_current_weather(city="Ha Noi"):
+    api_key = os.getenv("API_KEY")
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    # If running on local machine, try to load API key from .env file
+    if api_key is None and os.path.exists(".env"):
+        from dotenv import load_dotenv
+        load_dotenv()
+        api_key = os.getenv("API_KEY")
 
+    if api_key is None:
+        raise ValueError("API_KEY not found in environment variables or .env file")
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=imperial'
     weather_data = requests.get(request_url).json()
 
     return weather_data
-
 
 if __name__ == "__main__":
     print('\n*** Get Current Weather Conditions ***\n')
 
     city = input("\nPlease enter a city name: ")
-
-    # Check for empty strings or string with only spaces
-    # This step is not required here
-    # if not bool(city.strip()):
-    #     city = "Kansas City"
 
     weather_data = get_current_weather(city)
 
